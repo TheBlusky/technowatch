8c9d18cf210f6d178e55f1cab400b0a25370ced4
Blusky <git@blusky.fr>
2015-05-22 20:11:37 +0200

    Adding dribble

--- technowatch.py
+++ technowatch.py
@@ -77,6 +77,35 @@ except IOError:
 build()
 
 
+def check_dribble():
+    rebuild = False
+    # requesting dribble
+    html_doc = requests.get('https://dribbble.com/shots?list=animated').content
+    soup = BeautifulSoup(html_doc)
+    for li in soup.find_all('li', {'class': 'group'}):
+        try:
+            if li.get('id') is not None:
+                key = "drib-" + li.get('id')
+                if key not in known_stories:
+                    link = "https://dribbble.com" + li.find("a", {'class': 'dribbble-link'}).get('href')
+                    img = li.find("noscript").find("img").get('src').replace('_teaser', '')
+                    item = {'title': li.find('strong').get_text(),
+                            'url': link,
+                            'by': li.find("a", {"class": 'url'}).get('title'),
+                            'crawledDate': datetime.datetime.now().replace(tzinfo=pytz.utc),
+                            'type': "dribble",
+                            'key': key,
+                            'desc': "li.find('strong').get_text()<br />"
+                                    "<a href='" + link + "'>"
+                                    "<img src='" + img + "' />"
+                                    "</a>"}
+                    known_stories[key] = item
+                    rebuild = True
+        except:
+            print "Dribble bugged..."
+    return rebuild
+
+
 def check_githubtrend():
     rebuild = False
     # requesting github + bs
